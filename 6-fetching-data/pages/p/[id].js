e108377f6dc20a8c0f8436ad9405bd32cd8bc558
Dan Kiss <dan.kiss.contractor@move.com>
2020-01-06 13:54:49 -0800

    add fetching to [id].js note Link makes client side request, direct url visit is server side per next.js

--- 6-fetching-data/pages/p/[id].js
+++ 6-fetching-data/pages/p/[id].js
@@ -1,13 +1,38 @@
-import { useRouter } from 'next/router'
-import Layout from '../../components/MyLayout.js'
+import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-  const router = useRouter()
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  )
-}
+Post.getInitialProps = async function(context) {
+  console.log(13,context)
+  const {id} = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+  console.log(17,show);
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
+
+// import { useRouter } from 'next/router'
+// import Layout from '../../components/MyLayout.js'
+
+// export default function Post() {
+//   const router = useRouter()
+
+//   return (
+//     <Layout>
+//       <h1>{router.query.id}</h1>
+//       <p>This is the blog post content.</p>
+//     </Layout>
+//   )
+// }
